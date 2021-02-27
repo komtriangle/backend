dbe91ddfafa56f0c32e0f6623b7ec4595cfdfca8
komtriangle <56844333+komtriangle@users.noreply.github.com>
2021-02-27 11:52:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ origins = [
     "http://194.58.122.45:6537/",
     "https://194.58.122.45:6537/",
     "http://localhost",
-    "http://localhost:8000",
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
